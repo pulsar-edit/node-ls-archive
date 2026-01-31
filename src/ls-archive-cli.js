0098befd30d244269f694b00137b5f408cf45f4e
confused-Techie <dev@lhbasics.com>
2026-01-31 15:15:30 -0800

    Manual decaf `ls-archive-cli`

--- src/ls-archive-cli.js
+++ src/ls-archive-cli.js
@@ -1,22 +1,22 @@
-var archive, async, colors, optimist, path;
-
-path = require('path');
-
-async = require('async');
-
-colors = require('colors');
-
-optimist = require('optimist');
-
-archive = require('./ls-archive');
+const path = require("path");
+const async = require("async");
+const colors = require("colors");
+const optimist = require("optimist");
+const archive = require("./ls-archive.js");
 
 module.exports = function() {
-  var cli, files, queue;
-  cli = optimist.usage(`Usage: lsa [file ...]
+  const cli = optimist.usage(`Usage: lsa [file ...]
 
 List the files and folders inside an archive file.
 
-Supports .zip, .tar, .tar.gz, .tgz, .tar.bz2, .tbz and .tbz2 files.`).describe('colors', 'Enable colored output').default('colors', true).boolean('colors').describe('help', 'Show this message').alias('h', 'help').demand(1);
+Supports .zip, .tar, .tar.gz, .tgz, .tar.bz2, .tbz and .tbz2 files.`)
+    .describe('colors', 'Enable colored output')
+    .default('colors', true)
+    .boolean('colors')
+    .describe('help', 'Show this message')
+    .alias('h', 'help')
+    .demand(1);
+
   if (cli.argv.help) {
     cli.showHelp();
     return;
@@ -27,30 +27,33 @@ Supports .zip, .tar, .tar.gz, .tgz, .tar.bz2, .tbz and .tbz2 files.`).describe('
       red: 'stripColors'
     });
   }
-  queue = async.queue(function(archivePath, callback) {
+  let queue = async.queue(function(archivePath, callback) {
     return (function(archivePath) {
       return archive.list(archivePath, function(error, files) {
-        var file, i, index, len, prefix;
         if (error != null) {
           console.error(`Error reading: ${archivePath}`.red);
         } else {
+
           console.log(`${archivePath.cyan} (${files.length})`);
-          for (index = i = 0, len = files.length; i < len; index = ++i) {
-            file = files[index];
-            if (index === files.length - 1) {
+          for (let i = 0; i < files.length; i++) {
+            let file = files[i];
+            let prefix;
+            if (i === files.length - 1) {
               prefix = '\u2514\u2500\u2500 ';
             } else {
               prefix = '\u251C\u2500\u2500 ';
             }
             console.log(`${prefix}${file.getPath()}`);
           }
+
           console.log();
         }
         return callback();
       });
     })(archivePath);
   });
-  files = cli.argv._;
+
+  let files = cli.argv._;
   return files.forEach(function(file) {
     return queue.push(path.resolve(process.cwd(), file));
   });
