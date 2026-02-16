1d451858157628350929a3bcc3240619ad80a82d
confused-Techie <dev@lhbasics.com>
2026-02-15 22:09:44 -0800

    Decafe `zip-spec`

--- spec/zip-spec.js
+++ spec/zip-spec.js
@@ -1,9 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
 const archive = require('../src/ls-archive');
 const path = require('path');
 
@@ -19,12 +13,12 @@ describe("zip files", function() {
         const callback = (error, paths) => zipPaths = paths;
         archive.list(path.join(fixturesRoot, 'one-file.zip'), callback);
         waitsFor(() => zipPaths != null);
-        return runs(function() {
+        runs(function() {
           expect(zipPaths.length).toBe(1);
           expect(zipPaths[0].path).toBe('file.txt');
           expect(zipPaths[0].isDirectory()).toBe(false);
           expect(zipPaths[0].isFile()).toBe(true);
-          return expect(zipPaths[0].isSymbolicLink()).toBe(false);
+          expect(zipPaths[0].isSymbolicLink()).toBe(false);
         });
       });
 
@@ -33,20 +27,20 @@ describe("zip files", function() {
         const callback = (error, paths) => zipPaths = paths;
         archive.list(path.join(fixturesRoot, 'one-folder.zip'), callback);
         waitsFor(() => zipPaths != null);
-        return runs(function() {
+        runs(function() {
           expect(zipPaths.length).toBe(1);
           expect(zipPaths[0].path).toBe('folder');
           expect(zipPaths[0].isDirectory()).toBe(true);
           expect(zipPaths[0].isFile()).toBe(false);
-          return expect(zipPaths[0].isSymbolicLink()).toBe(false);
+          expect(zipPaths[0].isSymbolicLink()).toBe(false);
         });
       });
 
-      return describe("when the tree option is set to true", () => it("returns archive entries nested under their parent directory", function() {
+      describe("when the tree option is set to true", () => it("returns archive entries nested under their parent directory", function() {
         let tree = null;
         archive.list(path.join(__dirname, 'fixtures', 'nested.zip'), {tree: true}, (error, files) => tree = files);
         waitsFor(() => tree != null);
-        return runs(function() {
+        runs(function() {
           expect(tree.length).toBe(2);
 
           expect(tree[0].getPath()).toBe('d1');
@@ -58,7 +52,7 @@ describe("zip files", function() {
 
           expect(tree[1].getPath()).toBe('da');
           expect(tree[1].children[0].getName()).toBe('db');
-          return expect(tree[1].children[1].getName()).toBe('fa.txt');
+          expect(tree[1].children[1].getName()).toBe('fa.txt');
         });
       }));
     });
@@ -69,27 +63,27 @@ describe("zip files", function() {
       const callback = error => pathError = error;
       archive.list(archivePath, callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
 
-    return describe("when the archive path isn't a valid zip file", () => it("calls back with an error", function() {
+    describe("when the archive path isn't a valid zip file", () => it("calls back with an error", function() {
       const archivePath = path.join(fixturesRoot, 'invalid.zip');
       let pathError = null;
       const callback = error => pathError = error;
       archive.list(archivePath, callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
   });
 
-  return describe(".readFile()", function() {
+  describe(".readFile()", function() {
     describe("when the path exists in the archive", () => it("calls back with the contents of the given path", function() {
       const archivePath = path.join(fixturesRoot, 'one-file.zip');
       let pathContents = null;
       const callback = (error, contents) => pathContents = contents;
       archive.readFile(archivePath, 'file.txt', callback);
       waitsFor(() => pathContents != null);
-      return runs(() => expect(pathContents.toString()).toBe('hello\n'));
+      runs(() => expect(pathContents.toString()).toBe('hello\n'));
     }));
 
     describe("when the path does not exist in the archive", () => it("calls back with an error", function() {
@@ -98,7 +92,7 @@ describe("zip files", function() {
       const callback = (error, contents) => pathError = error;
       archive.readFile(archivePath, 'not-a-file.txt', callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
 
     describe("when the archive path does not exist", () => it("calls back with an error", function() {
@@ -107,7 +101,7 @@ describe("zip files", function() {
       const callback = (error, contents) => pathError = error;
       archive.readFile(archivePath, 'not-a-file.txt', callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
 
     describe("when the archive path isn't a valid zip file", () => it("calls back with an error", function() {
@@ -116,7 +110,7 @@ describe("zip files", function() {
       const callback = (error, contents) => pathError = error;
       archive.readFile(archivePath, 'invalid.txt', callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
 
     describe("when the path is a folder", () => it("calls back with an error", function() {
@@ -125,16 +119,16 @@ describe("zip files", function() {
       const callback = (error, contents) => pathError = error;
       archive.readFile(archivePath, `folder${path.sep}`, callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
 
-    return describe("when the archive contains nested directories", () => it("calls back with the contents of the given path", function() {
+    describe("when the archive contains nested directories", () => it("calls back with the contents of the given path", function() {
       const archivePath = path.join(fixturesRoot, 'nested.zip');
       let pathContents = null;
       const callback = (error, contents) => pathContents = contents;
       archive.readFile(archivePath, `d1${path.sep}d2${path.sep}f1.txt`, callback);
       waitsFor(() => pathContents != null);
-      return runs(() => expect(pathContents.toString()).toBe(''));
+      runs(() => expect(pathContents.toString()).toBe(''));
     }));
   });
 });
