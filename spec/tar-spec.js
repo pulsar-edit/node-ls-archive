e8a2948a094413621a4800d18e1a6113b52547e0
confused-Techie <dev@lhbasics.com>
2026-02-15 22:08:45 -0800

    Decafe `tar-spec`

--- spec/tar-spec.js
+++ spec/tar-spec.js
@@ -1,9 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
 const archive = require('../src/ls-archive');
 const path = require('path');
 
@@ -19,12 +13,12 @@ describe("tar files", function() {
         const callback = (error, paths) => tarPaths = paths;
         archive.list(path.join(fixturesRoot, 'one-file.tar'), callback);
         waitsFor(() => tarPaths != null);
-        return runs(function() {
+        runs(function() {
           expect(tarPaths.length).toBe(1);
           expect(tarPaths[0].path).toBe('file.txt');
           expect(tarPaths[0].isDirectory()).toBe(false);
           expect(tarPaths[0].isFile()).toBe(true);
-          return expect(tarPaths[0].isSymbolicLink()).toBe(false);
+          expect(tarPaths[0].isSymbolicLink()).toBe(false);
         });
       });
 
@@ -33,21 +27,21 @@ describe("tar files", function() {
         const callback = (error, paths) => tarPaths = paths;
         archive.list(path.join(fixturesRoot, 'one-folder.tar'), callback);
         waitsFor(() => tarPaths != null);
-        return runs(function() {
+        runs(function() {
           expect(tarPaths.length).toBe(1);
           expect(tarPaths[0].path).toBe('folder');
           expect(tarPaths[0].isDirectory()).toBe(true);
           expect(tarPaths[0].isFile()).toBe(false);
-          return expect(tarPaths[0].isSymbolicLink()).toBe(false);
+          expect(tarPaths[0].isSymbolicLink()).toBe(false);
         });
       });
 
-      return describe("when the tree option is set to true", function() {
+      describe("when the tree option is set to true", function() {
         describe("when the archive has no directories entries", () => it("returns archive entries nested under their parent directory", function() {
           let tree = null;
           archive.list(path.join(__dirname, 'fixtures', 'no-dir-entries.tgz'), {tree: true}, (error, files) => tree = files);
           waitsFor(() => tree != null);
-          return runs(function() {
+          runs(function() {
             expect(tree.length).toBe(1);
             expect(tree[0].getName()).toBe('package');
             expect(tree[0].getPath()).toBe('package');
@@ -67,15 +61,15 @@ describe("tar files", function() {
             expect(tree[0].children[4].children[2].getName()).toBe('ls-archive.js');
             expect(tree[0].children[4].children[2].getPath()).toBe(path.join('package', 'lib', 'ls-archive.js'));
             expect(tree[0].children[4].children[3].getName()).toBe('reader.js');
-            return expect(tree[0].children[4].children[3].getPath()).toBe(path.join('package', 'lib', 'reader.js'));
+            expect(tree[0].children[4].children[3].getPath()).toBe(path.join('package', 'lib', 'reader.js'));
           });
         }));
 
-        return describe("when the archive has multiple directories at the root", () => it("returns archive entries nested under their parent directory", function() {
+        describe("when the archive has multiple directories at the root", () => it("returns archive entries nested under their parent directory", function() {
           let tree = null;
           archive.list(path.join(__dirname, 'fixtures', 'nested.tar'), {tree: true}, (error, files) => tree = files);
           waitsFor(() => tree != null);
-          return runs(function() {
+          runs(function() {
             expect(tree.length).toBe(2);
 
             expect(tree[0].getPath()).toBe('d1');
@@ -87,7 +81,7 @@ describe("tar files", function() {
 
             expect(tree[1].getPath()).toBe('da');
             expect(tree[1].children[0].getName()).toBe('db');
-            return expect(tree[1].children[1].getName()).toBe('fa.txt');
+            expect(tree[1].children[1].getName()).toBe('fa.txt');
           });
         }));
       });
@@ -99,27 +93,27 @@ describe("tar files", function() {
       const callback = error => pathError = error;
       archive.list(archivePath, callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
 
-    return describe("when the archive path isn't a valid tar file", () => it("calls back with an error", function() {
+    describe("when the archive path isn't a valid tar file", () => it("calls back with an error", function() {
       const archivePath = path.join(fixturesRoot, 'invalid.tar');
       let pathError = null;
       const callback = error => pathError = error;
       archive.list(archivePath, callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
   });
 
-  return describe(".readFile()", function() {
+  describe(".readFile()", function() {
     describe("when the path exists in the archive", () => it("calls back with the contents of the given path", function() {
       const archivePath = path.join(fixturesRoot, 'one-file.tar');
       let pathContents = null;
       const callback = (error, contents) => pathContents = contents;
       archive.readFile(archivePath, 'file.txt', callback);
       waitsFor(() => pathContents != null);
-      return runs(() => expect(pathContents.toString()).toBe('hello\n'));
+      runs(() => expect(pathContents.toString()).toBe('hello\n'));
     }));
 
     describe("when the path does not exist in the archive", () => it("calls back with an error", function() {
@@ -128,7 +122,7 @@ describe("tar files", function() {
       const callback = (error, contents) => pathError = error;
       archive.readFile(archivePath, 'not-a-file.txt', callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
 
     describe("when the archive path does not exist", () => it("calls back with an error", function() {
@@ -137,7 +131,7 @@ describe("tar files", function() {
       const callback = (error, contents) => pathError = error;
       archive.readFile(archivePath, 'not-a-file.txt', callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
 
     describe("when the archive path isn't a valid tar file", () => it("calls back with an error", function() {
@@ -146,16 +140,16 @@ describe("tar files", function() {
       const callback = (error, contents) => pathError = error;
       archive.readFile(archivePath, 'invalid.txt', callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
 
-    return describe("when the path is a folder", () => it("calls back with an error", function() {
+    describe("when the path is a folder", () => it("calls back with an error", function() {
       const archivePath = path.join(fixturesRoot, 'one-folder.tar');
       let pathError = null;
       const callback = (error, contents) => pathError = error;
       archive.readFile(archivePath, `folder${path.sep}`, callback);
       waitsFor(() => pathError != null);
-      return runs(() => expect(pathError.message.length).toBeGreaterThan(0));
+      runs(() => expect(pathError.message.length).toBeGreaterThan(0));
     }));
   });
 });
